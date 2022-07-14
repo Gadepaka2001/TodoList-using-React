f2002852decc872e77753ee3c9c69880917f2457
Gadepaka2001 <gadepaka@appsforbharat.com>
2022-07-14 15:16:26 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Main1 from './Components/Main1';
+
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Main1 />
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
