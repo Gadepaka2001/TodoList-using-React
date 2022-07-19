8e9695492dd48ddf67112689e57292629a08db64
Gadepaka2001 <gadepaka@appsforbharat.com>
2022-07-19 11:15:49 +0530

    Second commit

--- src/Components/TodoLists.js
+++ src/Components/TodoLists.js
@@ -4,90 +4,90 @@ import Filters from './Filters';
 
 export default function TodoLists() {
   //In 'ALL' State we store todo lists.
-  const [ALL, setALL] = useState([{'Sno':1,'input':false,'title':'Learning React Tutorial','status':'completed'},{'Sno':2,'input':false,'title':'Tic Tac Toe using React - Assignment','status':'completed'},{'Sno':3,'input':false,'title':'Todos List using React - Assignment','status':'active'},{'Sno':4,'input':false,'title':'Need to learn CSS advanced','status':'active'},{'Sno':5,'input':false,'title':'Need to do some projects','status':'active'}]);
+  const [ALL, setALL] = useState([{'serialNumber':1,'input':false,'title':'Learning React Tutorial','status':'completed'},{'serialNumber':2,'input':false,'title':'Tic Tac Toe using React - Assignment','status':'completed'},{'serialNumber':3,'input':false,'title':'Todos List using React - Assignment','status':'active'},{'serialNumber':4,'input':false,'title':'Need to learn CSS advanced','status':'active'},{'serialNumber':5,'input':false,'title':'Need to do some projects','status':'active'}]);
   //In 'CHECK' state is used for filtering. By default it is in 'ALL'.
   const [CHECK, setCHECK] = useState('ALL');
   //In 'COLOR' we store background colors of 'ALL','Active' & 'Completed' in a list.
   const [COLOR, setCOLOR] = useState(['black','blue','blue'])
 
-  function makeUnderLine(Item) {
-    let ID = Item.target.value;
-    let Allarr = [...ALL];
-    for(let i=0;i<Allarr.length;i++) {
-        if (JSON.stringify(Allarr[i].Sno) === ID) {
-            if (Item.target.checked === true) {
-              Allarr[i].status = 'completed';
+  function makeUnderLine(item) {
+    let value = item.target.value;
+    let arrAll = [...ALL];
+    for(let i=0;i<arrAll.length;i++) {
+        if (JSON.stringify(arrAll[i].serialNumber) === value) {
+            if (item.target.checked === true) {
+              arrAll[i].status = 'completed';
             } else {
-              Allarr[i].status = 'active';
-              Item.target.checked = false;
+              arrAll[i].status = 'active';
+              item.target.checked = false;
             }
           }
       }
-    setALL(Allarr);
+    setALL(arrAll);
   }
 
-  function Delete(Item) {
+  function deleteItem(item) {
     let arr = [...ALL]
     for(let i=0;i<arr.length;i++) {
-      if (arr[i].Sno === Item) {
+      if (arr[i].serialNumber === item) {
         arr.splice(i,1);
       }
     }
     setALL(arr);
   }
 
-  function imDoubleClicked(Item) {
-    let ID = Item.target.id;
+  function imDoubleClicked(item) {
+    let id = item.target.id;
     let arr = [...ALL];
     for(let i=0;i<arr.length;i++) {
-      if (JSON.stringify(arr[i].Sno) === ID) {
+      if (JSON.stringify(arr[i].serialNumber) === id) {
         arr[i].input = true;
       }
     }
     setALL(arr);
   }
 
-  function AppendToList(S,Num) {
-    let I = document.getElementById(S);
+  function appendToList(inputId,serialNum) {
+    let inputTag = document.getElementById(inputId);
     let arr = [...ALL]
     let temp = {
-      'Sno':0,
+      'serialNumber':0,
       'title':'',
       'status':'active',
       'input':false
     }
-    if (S === 'main-input') {
-      temp.Sno = arr.length+1;
+    if (inputId === 'main-input') {
+      temp.serialNumber = arr.length+1;
     } else {
-      temp.Sno = Num;
+      temp.serialNumber = serialNum;
     }
-    temp.title = I.value;
-    let N = 50;
-    if (I.value.length <= N) {
-      if (S === 'main-input') {
+    temp.title = inputTag.value;
+    let n = 50; //restricting to take upto 50 letters only.
+    if (inputTag.value.length <= n) {
+      if (inputId === 'main-input') {
         arr.push(temp)
       } else {
-        arr[Num-1] = temp;
+        arr[serialNum-1] = temp;
       }
       setALL(arr)
     } else {
-      alert('Exceding '+JSON.stringify(N)+' Letters')
+      alert('Exceding '+JSON.stringify(n)+' Letters')
     }
   }
 
-  function ChangeCheckToAll() {
+  function changeCheckToAll() {
     let arr = ['black','blue','blue'];
     setCOLOR(arr);
     setCHECK('ALL');
   }
 
-  function ChangeCheckToActive() {
+  function changeCheckToActive() {
     let arr = ['blue','black','blue'];
     setCOLOR(arr);
     setCHECK('Active');
   }
 
-  function ChangeCheckToCompleted() {
+  function changeCheckToCompleted() {
     let arr = ['blue','blue','black'];
     setCOLOR(arr);
     setCHECK('Completed');
@@ -105,21 +105,21 @@ export default function TodoLists() {
     </div>
     <div className='container'>
       <input className='Input' type="text" id='main-input'/>
-      <span className='btn' onClick={()=>{AppendToList('main-input',0)}}>
+      <span className='btn' onClick={()=>{appendToList('main-input',0)}}>
         +
       </span>
     </div>
     <div>
-      <Filters CHECK = {CHECK} ALL = {ALL} makeUnderLine = {makeUnderLine} Delete = {Delete} imDoubleClicked = {imDoubleClicked} AppendToList = {AppendToList}/>;
+      <Filters CHECK = {CHECK} ALL = {ALL} makeUnderLine = {makeUnderLine} deleteItem = {deleteItem} imDoubleClicked = {imDoubleClicked} appendToList = {appendToList}/>;
     </div>
     <div className='filterIcons'>
-      <span id='all' style={{'backgroundColor':COLOR[0]}} onClick={()=>{ChangeCheckToAll()}}>
+      <span id='all' style={{'backgroundColor':COLOR[0]}} onClick={()=>{changeCheckToAll()}}>
         All
       </span>
-      <span id='active' style={{'backgroundColor':COLOR[1]}} onClick={()=>{ChangeCheckToActive()}}>
+      <span id='active' style={{'backgroundColor':COLOR[1]}} onClick={()=>{changeCheckToActive()}}>
         Active
       </span>
-      <span id='completed' style={{'backgroundColor':COLOR[2]}} onClick={()=>{ChangeCheckToCompleted()}}>
+      <span id='completed' style={{'backgroundColor':COLOR[2]}} onClick={()=>{changeCheckToCompleted()}}>
         Completed
       </span>
     </div>
